0e8244f294fd3921b5948a956839be4445fbbce3
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-02 22:03:17 -0500

    initial project setup

---
+++ dj/pages/api/events/[slug].js
@@ -0,0 +1,5 @@
+export default function MyEvent() {
+  return (
+    <div><h2>My Event</h2></div>
+  )
+}
---
+++ dj/pages/api/events/add.js
@@ -0,0 +1,5 @@
+export default function add() {
+  return (
+    <div><h2>Add Event</h2></div>
+  )
+}
