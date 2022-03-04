77ef904b9c9bcc44c12947206ccbf7da2ed11f80
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-03 22:11:18 -0500

    added useRouter

--- dj/pages/events/[slug].js
+++ dj/pages/events/[slug].js
@@ -1,4 +1,8 @@
+import {useRouter} from 'next/router';
+
 export default function MyEvent() {
+  const router = useRouter();
+  console.log(router);
   return (
     <div><h2>My Event</h2></div>
   )
