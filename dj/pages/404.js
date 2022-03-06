6856f6c2e6c5db93146a5e0aa0dfd896f3103358
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-05 20:47:19 -0500

    making 404 page

---
+++ dj/pages/404.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+
+export default function NotFoundPage() {
+  return (
+      <Layout title="Page Not Found">
+        <div className={styles.error}>
+            <h1>404</h1>
+            <h4>Sorry nothing here</h4>
+            <Link href="/">Go Home</Link>
+        </div>
+      </Layout>
+  )
+}
