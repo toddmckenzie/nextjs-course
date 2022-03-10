ec802ab952333336b022359c2c1535b0c3af7c7e
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-09 19:39:05 -0500

    learning getStaticProps/getServersideprops

--- dj/pages/index.js
+++ dj/pages/index.js
@@ -1,15 +1,25 @@
 import Layout from '@/components/Layout'
 import Link from 'next/link'
+import { API_URL } from '@/config/index'
 
 
-export default function HomePage() {
+export default function HomePage({events}) {
+  console.log(events)
   return (
     <Layout title='DJ Events'>
    
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-      <p>Homepage</p>
+      <h1>UpComingEvents</h1>
       
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {events}
+  }
 }
