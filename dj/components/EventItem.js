514f1cedcddd1db81f95d45ff211fdcd134e83d1
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-10 19:19:07 -0500

    getStaticpaths and getStaticprops myevent

--- dj/components/EventItem.js
+++ dj/components/EventItem.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import styles from '@/styles/EventItems.module.css'
 
 export default function EventItem({ evt }) {
-console.log(evt)
   return (
     <div className={styles.event}>
         <div className={styles.img}>
--- dj/pages/events/[slug].js
+++ dj/pages/events/[slug].js
@@ -1,11 +1,38 @@
-import {useRouter} from 'next/router';
+import {useRouter} from 'next/router'
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
+import { resolveHref } from 'next/dist/shared/lib/router/router';
 
-
-export default function MyEvent() {
+export default function MyEvent({evt}) {
   const router = useRouter();
   console.log(router);
   return (
-    <Layout title="My Event"><h2>My Event</h2></Layout>
+    <Layout title="My Event"><h2>{evt.name}</h2></Layout>
   )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map(evt => ({
+    params: { slug: evt.slug }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+
+export async function getStaticProps({ params: {slug}}){
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0]
+    }
+  }
 }
